08c0fb552e4a0ddfecb3dd014f5d171c74ca85af
alexsvianzon <alexsvianzon@gmail.com>
2026-05-02 09:22:50 -0500

    add a debug mode to test filesystem interactions, print program info

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,16 @@
 //constants.rs
+use std::env;
 
+// debugging mode 
+// prints OS, version, and name,
+// tests filesystem operations
+pub const DEBUG: bool = true;
+
+// program variables
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const NAME: &str = env!("CARGO_PKG_NAME");
+pub const OS: &str = env::consts::OS;
+
+// filesystem variables
+pub const APPDATA_DIR: &str = "./{NAME}/";
+pub const TEST_DIR: &str = "./test/";
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 // main.rs
-use clap::{Command, arg, command};
+
 mod commands;
 mod constants;
+
+use clap::{Command, arg, command};
+use std::fs;
+use std::path::Path;
 use crate::constants::VERSION;
 use crate::constants::NAME;
+use crate::constants::OS;
 
 fn main() {
+    if constants::DEBUG {
+        println!("DEBUG is enabled. Version is {VERSION} of {NAME} app. OS is {OS}.");
+
+        let test_dir_path = Path::new(constants::TEST_DIR);
+        let test_dir_display = test_dir_path.display();
+        let result = fs::create_dir_all(test_dir_path);
+        match result {
+            Ok(_) => (),
+            Err(error) => panic!("Could not create all directories to path {test_dir_display} because of error: {error:?}"),
+        }
+        
+        let test_file_path = [constants::TEST_DIR, "test.txt"].concat();
+        let result = fs::write(&test_file_path, "This file was generated while debugging {NAME}.");
+        match result {
+            Ok(_) => (),
+            Err(error) => panic!("Could not write to the test file at path {test_file_path} because of error: {error:?}"),
+        }
+
+        println!("Finished DEBUG! Attempted write to a test file at {test_dir_display}.");
+    }
+
     let matches = command!()
         .subcommand_required(true)
         .version(VERSION)
