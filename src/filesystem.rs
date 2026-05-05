4c834d0cc547d8163218e3d058928c3af5bba6de
alexsvianzon <alexsvianzon@gmail.com>
2026-05-05 17:30:49 -0500

    add a unit test, not working

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -23,4 +23,25 @@ impl File {
 
         self.content = string_content;
     }
+
+    pub fn write(content: String) {
+        let write_result = fs::write(self.path, content);
+        self.content = match write_result {
+            Ok(_) => content,
+            Err(error) => panic!("Could not write to file because of {error:?}"),
+        };
+    }
+}
+
+#[test]
+fn file_struct_test() {
+    let content = "This message was generated while testing 'filesystem.rs'";
+    let path_str = "./test/unittest.txt";
+    let path = Path::new(path_str);
+    let mut file: File = File::new(path_str);
+
+    assert_eq!(file.path, path);
+
+    file.write(content.to_string());
+    assert_eq!(file::read(), content);
 }
