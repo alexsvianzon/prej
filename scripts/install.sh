788a66e0994f9378c98f6af1cdae0f0d686cd508
alexsvianzon <alexsvianzon@gmail.com>
2026-06-04 11:20:47 -0500

    script ready for zsh, beta branch ready for release
    exit
    clear

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,16 +14,16 @@ esac
 
 case $SHELL in
   *zsh)
-    curl -o /usr/local/bin/prej.zsh https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/master/scripts/prej.zsh
-    curl -o /usr/local/bin/prej https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/master/scripts/prej.zsh
+    sudo curl -o /usr/local/bin/prej.zsh https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/master/scripts/prej.zsh
+    sudo curl -o /usr/local/bin/prej https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/master/build/prej
 
     if ! grep -q "# --> prej setup -->" ~/.zshrc; then
         cat <<EOF >> ~/.zshrc
 
-      # --> prej setup -->
-      source /usr/local/bin/prej.zsh
-      # <-- prej setup <--
-      EOF
+# --> prej setup -->
+source /usr/local/bin/prej.zsh
+# <-- prej setup <--
+EOF
     fi
 
     ;;
