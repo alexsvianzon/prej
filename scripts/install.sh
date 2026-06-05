345b4a6f881768619260990b41bc098a25beb3df
pixel.pine <alexsvianzon@gmail.com>
2026-06-04 22:18:06 -0500

    Update download URLs to beta branch for prej

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,8 +16,8 @@ sudo mkdir -p /usr/local/bin
 
 case $SHELL in
   *zsh)
-    sudo curl -o /usr/local/bin/prej.zsh https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/master/scripts/prej.zsh
-    sudo curl -o /usr/local/bin/prej https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/master/build/prej
+    sudo curl -o /usr/local/bin/prej.zsh https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/beta/scripts/prej.zsh
+    sudo curl -o /usr/local/bin/prej https://raw.githubusercontent.com/alexsvianzon/prej/refs/heads/beta/build/prej
 
     if ! grep -q "# --> prej setup -->" ~/.zshrc; then
         cat <<EOF >> ~/.zshrc
